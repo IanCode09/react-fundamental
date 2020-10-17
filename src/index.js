236583dea3a281c30f867183115b8571ce94228f
IanCode09 <ianlombu@protonmail.com>
2020-10-17 14:57:41 +0700

    Event Basic

--- src/index.js
+++ src/index.js
@@ -22,18 +22,27 @@ function BookList() {
     <section className='booklist'>
       {books.map((book) => {
         return <Book key={book.id} {...book} />
-        
       })}
     </section>
   )
 }
 
 const Book = ({ img, title, author }) => {
+  const clickHandler = () => {
+    console.log('You Hit Me!!!!')
+  }
+
+  const fontBoldHandler = (author, title) => {
+    alert(`Title: ${title}, Author: ${author}`);
+  }
+
   return (
     <article className='book'>
       <img src={img} alt='book1' style={{width: '200px'}} />
       <h2>{title}</h2>
       <h3>{author}</h3>
+      <button className='btn-primary' type='button' onClick={clickHandler}>Click Me</button>
+      <button className='btn-secondary' type='button' onClick={() => fontBoldHandler(author, title)}>Click Me</button>
     </article>
   )
 }
